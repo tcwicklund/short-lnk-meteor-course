2a04aeebc6cef25b55d353bff324af846a46b8bf
Travis Wicklund <tcwicklund@gmail.com>
2020-04-23 00:43:41 -0400

    Update NotFound header text

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -5,7 +5,7 @@ export default () => {
   return (
     <div className="boxed-view">
       <div className="boxed-view__box">
-        <h1>Page Not Found</h1>
+        <h1>Status 404 - Page Not Found</h1>
         <p>Hmmm, we're unable to find that page.</p>
         <Link to="/" className="button button--link">Head Home</Link>
       </div>
